33cd8ed34379932b9dbb5a6f0c3db38ac904c5e2
hpozzy <oscargallegostapia@gmail.com>
2020-09-13 15:07:04 -0700

    sundaypm

--- main.js
+++ main.js
@@ -1,5 +1,63 @@
-console.log(items)
+//
+document.getElementById("accessoriesShow").onmouseover = function () {
+  document.getElementById("dropC2Hide").style.visibility = "visible"
+}
 
-for (let itemList in items) {
-  console.log(itemList)
+document.getElementById("jewelryAnchor").onmouseover = function () {
+  document.getElementById("dropC2Hide").style.visibility = "visible"
+}
+
+document.getElementById("firstAnchor").onmouseover = function () {
+  document.getElementById("dropC2Hide").style.visibility = "visible"
+}
+
+document.getElementById("accessoriesShow").onmouseout = function () {
+  document.getElementById("dropC2Hide").style.visibility = "hidden"
+}
+
+document.getElementById("bagsShow").onmouseover = function () {
+  document.getElementById("dropC3").style.visibility = "visible"
+}
+
+document.getElementById("bagsShow").onmouseout = function () {
+  document.getElementById("dropC3").style.visibility = "hidden"
+}
+
+document.getElementById("necklacesShow").onmouseover = function () {
+  document.getElementById("dropC4").style.visibility = "visible"
+}
+
+document.getElementById("necklacesShow").onmouseout = function () {
+  document.getElementById("dropC4").style.visibility = "hidden"
+}
+
+document.getElementById("ringsShow").onmouseover = function () {
+  document.getElementById("dropC5").style.visibility = "visible"
+}
+
+document.getElementById("ringsShow").onmouseout = function () {
+  document.getElementById("dropC5").style.visibility = "hidden"
+}
+
+document.getElementById("ear-rings").onmouseover = function () {
+  document.getElementById("dropC6").style.visibility = "visible"
+}
+
+document.getElementById("ear-rings").onmouseout = function () {
+  document.getElementById("dropC6").style.visibility = "hidden"
+}
+
+document.getElementById("braceletShow").onmouseover = function () {
+  document.getElementById("dropC7").style.visibility = "visible"
+}
+
+document.getElementById("braceletShow").onmouseout = function () {
+  document.getElementById("dropC7").style.visibility = "hidden"
+}
+document.getElementById("bodyj").onmouseover = function () {
+  document.getElementById("dropC8").style.visibility = "visible"
+}
+
+document.getElementById("bodyj").onmouseout = function () {
+  document.getElementById("dropC8").style.visibility = "hidden"
 }
