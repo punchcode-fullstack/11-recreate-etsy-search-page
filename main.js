3e97b93583a97b2f6ac1efb97be1db4e4b066ecc
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-09-13 10:58:01 -0700

    update branch

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const html = items.results.map(item => {
+const html = items.results
+  .map((item) => {
     return `
     <div class= "products">
           <img src= ${unescape(item.Images[0].url_170x135)}/>
         <h4>${item.title}</h4>
-        <div class= "desc">
+        <div class= "board">
         <p>${item.Shop.shop_name}</p>
-        <p>Views (${item.views})</p>
-        <p class= "price">$${item.price}</p>
+        <p><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span>(${
+          item.views
+        })</p>
+        <p class= "price">$${item.price}<span id="shipping"> FREE shipping</span></p>
+        
         </div>
     </div>
-    `
-}).join('')
+    `;
+  })
+  .join("");
 
-document.querySelector("#products").innerHTML = html
+document.querySelector("#products").innerHTML = html;
+
 
