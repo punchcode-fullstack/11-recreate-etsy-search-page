acc29a2ee8c5f9faa91f11f33817bff2c8358fdc
Will Stoddard <wstoddard89@gmail.com>
2020-09-12 23:13:18 -0700

    made drop downs and js

---
+++ main.js
@@ -0,0 +1,71 @@
+// 
+document.getElementById('accessoriesShow').onmouseover = function(){
+  document.getElementById('dropC2Hide').style.visibility = 'visible';
+}
+
+document.getElementById('jewelryAnchor').onmouseover = function(){
+  document.getElementById('dropC2Hide').style.visibility = 'visible';
+}
+
+document.getElementById('firstAnchor').onmouseover = function(){
+  document.getElementById('dropC2Hide').style.visibility = 'visible';
+}
+
+document.getElementById('accessoriesShow').onmouseout = function(){
+  document.getElementById('dropC2Hide').style.visibility = 'hidden';
+}
+
+document.getElementById('bagsShow').onmouseover = function(){
+  document.getElementById('dropC3').style.visibility = 'visible';
+}
+document.getElementById('dropC3').onmouseover = function(){
+  document.getElementById('dropC3').style.visibility = 'visible';
+}
+
+document.getElementById('bagsShow').onmouseout = function(){
+  document.getElementById('dropC3').style.visibility = 'hidden';
+}
+
+document.getElementById('dropC3').onmouseout = function(){
+  document.getElementById('dropC2Hide').style.visibility = 'hidden';
+}
+
+document.getElementById('necklacesShow').onmouseover = function(){
+  document.getElementById('dropC4').style.visibility = 'visible';
+}
+
+document.getElementById('necklacesShow').onmouseout = function(){
+  document.getElementById('dropC4').style.visibility = 'hidden';
+}
+
+document.getElementById('ringsShow').onmouseover = function(){
+  document.getElementById('dropC5').style.visibility = 'visible';
+}
+
+document.getElementById('ringsShow').onmouseout = function(){
+  document.getElementById('dropC5').style.visibility = 'hidden';
+}
+
+document.getElementById('earringsShow').onmouseover = function(){
+  document.getElementById('dropC6').style.visibility = 'visible';
+}
+
+document.getElementById('earringsShow').onmouseout = function(){
+  document.getElementById('dropC6').style.visibility = 'hidden';
+}
+
+document.getElementById('braceletsShow').onmouseover = function(){
+  document.getElementById('dropC7').style.visibility = 'visible';
+}
+
+document.getElementById('braceletsShow').onmouseout = function(){
+  document.getElementById('dropC7').style.visibility = 'hidden';
+}
+
+document.getElementById('bodyjewelryShow').onmouseover = function(){
+  document.getElementById('dropC8').style.visibility = 'visible';
+}
+
+document.getElementById('bodyjewelryShow').onmouseout = function(){
+  document.getElementById('dropC8').style.visibility = 'hidden';
+}
